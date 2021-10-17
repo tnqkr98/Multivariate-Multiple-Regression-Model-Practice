e932eabcc7418e2bb3f85d85d9f2aafd040c0fd1
DongHan <tnqkr98@naver.com>
2021-10-17 23:14:23 +0900

    torch regression model

--- dataProcessing.py
+++ dataProcessing.py
@@ -2,91 +2,6 @@ import pandas as pd
 import os
 
 
-def make_one_file(merge_dir, individual_dir):
-    resultfile = pd.DataFrame()
-    merge_files = os.listdir(merge_dir)
-    for file in merge_files:
-        df_merge = pd.read_excel(merge_dir + "/" + file)
-        df_individual_sheet2 = pd.read_excel(individual_dir + "/" + file[4:], sheet_name=1)
-        row_size = df_merge['csd'].size
-        df_merge = df_merge.drop(['time'], axis=1)  # 절대 시간열 제거
-        df_merge['time'] = [i for i in range(row_size)]  # 상대 시간열 추가
-
-        # column - awake & asleep
-        val_list = list()
-        for i in range(row_size):
-            if df_merge['level'][i] == 'wake' or df_merge['level'][i] == 'awake' or df_merge['level'][i] == 'restless':
-                val_list.append('awake')
-            else:
-                val_list.append('asleep')
-        df_merge['state'] = val_list
-
-        # column - age
-        value = df_individual_sheet2['age'][0]
-        df_merge['age'] = [value for i in range(row_size)]
-
-        # column - sex (Binary)
-        value = df_individual_sheet2['sex'][0]
-        df_merge['sex'] = [value for i in range(row_size)]
-
-        # column - height
-        value = df_individual_sheet2['height'][0]
-        df_merge['height'] = [value for i in range(row_size)]
-
-        # column - weight
-        value = df_individual_sheet2['weight'][0]
-        df_merge['weight'] = [value for i in range(row_size)]
-
-        # column - disease (5 Category) -> 처리잘해야할듯
-        value = df_individual_sheet2['q5'][0]
-        df_merge['disease'] = [value for i in range(row_size)]
-
-        # column - depressive
-        value = df_individual_sheet2['q9'][0]
-        df_merge['depressive'] = [value for i in range(row_size)]
-
-        # column - disorder (Binary)        // 1 : yes,  2: no
-        value = df_individual_sheet2['q10'][0]
-        df_merge['disorder'] = [value for i in range(row_size)]
-
-        # column - media
-        value = df_individual_sheet2['q12'][0] - 1
-        val_list = [15, 45, 90, 150, 210, 15, 45]
-        df_merge['media'] = [val_list[value] for i in range(row_size)]
-
-        # column - liquor
-        value = df_individual_sheet2['dq1'][0] - 1
-        val_list = [0, 14, 42, 85, 114]
-        df_merge['liquor'] = [val_list[value] for i in range(row_size)]
-
-        # column - smoke
-        value = df_individual_sheet2['dq2'][0] - 1
-        val_list = [0, 7, 22, 37, 47]
-        df_merge['smoke'] = [val_list[value] for i in range(row_size)]
-
-        # column - caffeine
-        value = df_individual_sheet2['dq3'][0] - 1
-        val_list = [0, 189, 30, 43, 65]
-        df_merge['caffeine'] = [val_list[value] for i in range(row_size)]
-
-        # column - exercise
-        value = df_individual_sheet2['dq4'][0] - 1
-        val_list = [0, 15, 45, 90, 150]
-        df_merge['exercise'] = [val_list[value] for i in range(row_size)]
-
-        # column - stress
-        value = df_individual_sheet2['dq5'][0]
-        df_merge['stress'] = [value for i in range(row_size)]
-
-        # column - nap (Binary)
-        value = df_individual_sheet2['dq6'][0]
-        df_merge['nap'] = [value for i in range(row_size)]
-        resultfile = pd.concat([resultfile, df_merge])
-
-    print(resultfile)
-    return resultfile
-
-
 def make_one_file_remain_category(merge_dir, individual_dir):
     resultfile = pd.DataFrame()
     merge_files = os.listdir(merge_dir)
@@ -125,6 +40,19 @@ def make_one_file_remain_category(merge_dir, individual_dir):
 
         # column - disease (5 Category) -> 처리잘해야할듯
         value = df_individual_sheet2['q5'][0]
+        value = str(value)[0]
+        if value == '0':
+            value = "none"
+        elif value == '1':
+            value = "hypertensive"
+        elif value == '2':
+            value = "diabetes"
+        elif value == '3':
+            value = "liverDisease"
+        elif value == '4':
+            value = "tuberculosis"
+        elif value == '5':
+            value = "mental"
         df_merge['disease'] = [value for i in range(row_size)]
 
         # column - depressive
--- regression.py
+++ regression.py
@@ -4,11 +4,23 @@ import torchvision
 import torch.nn as nn
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, TensorDataset, random_split
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+
+torch.manual_seed(1)
+
+
+class LinearRegression(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(LinearRegression,self).__init__()
+        self.linear = nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+        return self.linear(x)
 
 
 def remove_outliers(df, column_name, lower, upper):
@@ -26,10 +38,57 @@ sample_data = pd.get_dummies(sample_data)                       # Embedding
 
 x_data = sample_data.iloc[:, 6:]
 y_data = sample_data.iloc[:, [1, 2, 3, 4, 5]]
-x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.33, random_state=42)
-batch_size = 128
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.1, random_state=42)
+x_data.info(verbose=True, show_counts=True)
+
+learning_rate = 0.02
+iteration_number = 1000
 
 sc_x = StandardScaler()
 x_train_scaled = sc_x.fit_transform(x_train)
 x_test_scaled = sc_x.transform(x_test)
 
+sc_y = StandardScaler()
+y_train_scaled = sc_y.fit_transform(y_train)
+y_test_scaled = sc_y.transform(y_test)
+
+x_train_scaled = np.array(x_train_scaled, dtype=np.float32)
+y_train_scaled = np.array(y_train_scaled, dtype=np.float32)
+x_test_scaled = np.array(x_test_scaled, dtype=np.float32)
+y_test_scaled = np.array(y_test_scaled, dtype=np.float32)
+
+inputs = torch.from_numpy(x_train_scaled)
+targets = torch.from_numpy(y_train_scaled)
+
+test_inputs = torch.from_numpy(x_test_scaled)
+test_targets = torch.from_numpy(y_test_scaled)
+
+input_dim = x_train.shape[1]
+output_dim = y_train.shape[1]
+
+model = LinearRegression(input_dim, output_dim)
+mse = nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+loss_list = []
+test_loss_list = []
+for iteration in range(iteration_number):
+
+    optimizer.zero_grad()
+    results = model(inputs)
+    loss = mse(results, targets)
+    loss.backward()
+    optimizer.step()
+    loss_list.append(loss.data)
+
+    test_results = model(test_inputs)
+    test_loss = mse(test_results, test_targets)
+    test_loss_list.append(test_loss.data)
+
+    if iteration % 50 == 0:
+        print('epoch %3d, trian loss : %f, test loss : %f ' % (iteration, loss.data, test_loss.data))
+
+plt.plot(range(iteration_number), loss_list, range(iteration_number), test_loss_list)
+plt.xlabel("Number of Iterations")
+plt.ylabel("Loss")
+plt.show()
